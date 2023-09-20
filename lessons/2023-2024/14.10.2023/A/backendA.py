04143abed74a0a03b10d2fd5ed291849aaa9d255
chellick <matsupus@gmail.com>
2023-09-20 09:45:08 +0300

    upd.

--- lessons/2023-2024/14.10.2023/A/backendA.py
+++ lessons/2023-2024/14.10.2023/A/backendA.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 class Point:
     def __init__(self, coords):
         self.coords = coords
@@ -14,20 +17,36 @@ def f(g, h):
     return g + h
 
 
-def A(start, goal, points):
-    current_f = f(vector_length(start, start), vector_length(start, goal))
+def A(start, goal, points: list):
     current_state = start
-    opened_points = []
+    current_f = f(vector_length(start, current_state), vector_length(current_state, goal))
+    opened_points = points
     closed_points = [start]
     
-    for point in points:
-        if f(vector_length(start, point), vector_length(point, goal)) < current_f:
-            current_f = f(vector_length(start, point), vector_length(point, goal))
-            current_state = point
-            
+    while len(opened_points) != 0:
+        for point in opened_points:
+            if f(vector_length(start, point), vector_length(point, goal)) <= current_f:
+                current_f = f(vector_length(start, point), vector_length(point, goal))
+                current_state = point
+                print(current_state)
         
-    
+        closed_points.append(current_state)
+        opened_points.pop(opened_points.index(current_state))
+        print(len(opened_points))
+        
+        
+    return current_state
 
-
+def plot_points(start, goal, points):
+    x = []
+    y = []
+    for i in points:
+        x.append(i.coords[0])
+        y.append(i.coords[1])
     
+    plt.scatter([start.coords[0], goal.coords[0]], [start.coords[1], goal.coords[1]])
+    plt.scatter(x, y)
+    plt.show()
     
+                
+            
--- lessons/2023-2024/14.10.2023/A/testA.py
+++ lessons/2023-2024/14.10.2023/A/testA.py
@@ -3,6 +3,12 @@ import numpy as np
 
 
 start = Point([0, 0])
-end = Point([10, 10])
+goal = Point([10, 10])
 
-points = (Point[0], )
+points = [Point([1, 1]), Point([2, 3]), Point([4, 5]), Point([7, 8]), Point([10, 0])]
+plot_points(start, goal, points)
+
+print(f(vector_length(start, Point([1, 1])), vector_length(Point([1, 1]), goal)))
+print(f(vector_length(start, Point([2, 3])), vector_length(Point([2, 3]), goal)))
+
+# print(A(start, goal, points))
